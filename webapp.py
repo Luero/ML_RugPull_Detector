1aafc7278c6b2fe619c7806b7d1eb53423020819
Kseniia <kseniapodguzova@gmail.com>
2026-08-30 12:59:43 +0300

    Add a test web-page for future UI

---
+++ webapp.py
@@ -0,0 +1,15 @@
+# UI for the rug-pull detector
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Main page of UI
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
